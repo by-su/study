b6e3efb695a5e7bdf6a87c1cd59ea5217e54c06a
bysu <dobby.indev@gmail.com>
2025-10-16 18:33:54 +0900

    batch init

---
+++ batch-prac/src/main/kotlin/com/rootbly/batchprac/BatchPracApplication.kt
@@ -0,0 +1,11 @@
+package com.rootbly.batchprac
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BatchPracApplication
+
+fun main(args: Array<String>) {
+    runApplication<BatchPracApplication>(*args)
+}
---
+++ batch-prac/src/test/kotlin/com/rootbly/batchprac/BatchPracApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rootbly.batchprac
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BatchPracApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

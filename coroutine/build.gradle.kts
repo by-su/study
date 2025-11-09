23984f0364db939c82bb0f23df493aeec6aeb35d
bysu <dobby.indev@gmail.com>
2025-11-09 12:45:07 +0900

    [coroutine] 코루틴 연습 프로젝트 생성

---
+++ coroutine/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.1.21"
+}
+
+group = "com.rootbly"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}

3ab0d865c751553a1477c9d02cbff586c8a64851
bysu <dobby.indev@gmail.com>
2025-11-12 23:18:12 +0900

    [ Alg ] TwoSum (Hash)

---
+++ algorithm/python/TwoSum.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target: return [i, j]
+
+        return []
+
+class Solution2:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+
+        return []
+
+solution = Solution()
+result = solution.twoSum([2, 7, 11, 15], 9)
+print(f"Result: {result}")

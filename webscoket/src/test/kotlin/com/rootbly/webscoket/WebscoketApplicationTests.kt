fe62c8d7d3743ccf12d3dd10ab87de2d4bc930d7
bysu <dobby.indev@gmail.com>
2025-11-22 12:43:45 +0900

    [ websocket ] websocket project init

---
+++ webscoket/src/test/kotlin/com/rootbly/webscoket/WebscoketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rootbly.webscoket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebscoketApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
